8265d580f008f478706d244a39665be6a54b1963
AbelGebu <abel.gebu@gmail.com>
2026-08-20 00:11:10 +0400

    Add new model

---
+++ HospitalAppoinmentManagementSystem/HospitalAppoinmentManagementSystem/Models/Appointment.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HospitalAppoinmentManagementSystem.Models
+{
+    public class Appointment
+    {
+        [Key]
+        public int AppointmentId { get; set; }
+        public int PatientId { get; set; }
+        public int TimeSlotId { get; set; }
+        public string Status { get; set; } = "Pending"; // Pending, Approved, Cancelled
+        public virtual Patient? Patient { get; set; }
+        public virtual Timeslot? Timeslot { get; set; }
+    }
+}
---
+++ HospitalAppoinmentManagementSystem/HospitalAppoinmentManagementSystem/Models/HospitalDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using HospitalAppoinmentManagementSystem.Models;
+
+namespace HospitalAppoinmentManagementSystem.Models
+{
+    public class HospitalDbContext : DbContext
+    {
+        public HospitalDbContext(DbContextOptions<HospitalDbContext> options) : base(options){}
+
+        public DbSet<Doctor> Doctors { get; set; }
+        public DbSet<Patient> Patients { get; set; }
+        public DbSet<Timeslot> TimeSlots { get; set; }
+        public DbSet<Appointment> Appointments { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
