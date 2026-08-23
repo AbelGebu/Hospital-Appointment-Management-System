0b8aa870fcb0dfd754b1eca0ee0776bccde4d3da
AbelGebu <abel.gebu@gmail.com>
2026-08-23 19:21:48 +0400

    Updated data file

---
+++ HospitalAppoinmentManagementSystem/HospitalAppoinmentManagementSystem/Data/DbInitializer.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace HospitalAppoinmentManagementSystem.Data
+{
+    public class DbInitializer
+    {
+        public static async Task SeedRolesAndAdminAsync(IServiceProvider serviceProvider)
+        {
+            var roleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+            var userManager = serviceProvider.GetRequiredService<UserManager<IdentityUser>>();
+
+            // 1. Create Roles
+            string[] roleNames = { "Admin", "Doctor", "Patient" };
+            foreach (var roleName in roleNames)
+            {
+                if (!await roleManager.RoleExistsAsync(roleName))
+                {
+                    await roleManager.CreateAsync(new IdentityRole(roleName));
+                }
+            }
+
+            // 2. Create Default Admin
+            var adminEmail = "admin@hospital.com";
+            var adminUser = await userManager.FindByEmailAsync(adminEmail);
+            if (adminUser == null)
+            {
+                var admin = new IdentityUser
+                {
+                    UserName = adminEmail,
+                    Email = adminEmail,
+                    EmailConfirmed = true
+                };
+
+                var result = await userManager.CreateAsync(admin, "Admin@123");
+                if (result.Succeeded)
+                {
+                    await userManager.AddToRoleAsync(admin, "Admin");
+                }
+            }
+        }
+    }
+}
